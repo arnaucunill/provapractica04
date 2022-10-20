508b561a67d7cf873d953a8417bf4eab0f53b262
Arnau Cunill <arnau.cunill@gmail.com>
2022-10-20 12:47:04 +0200

    copia1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroeComponent } from './components/heroe/heroe.component';
+import { AboutComponent } from './components/about/about.component';
+
+const routes: Routes = [
+  {path : '', component:HomeComponent},
+  {path : 'home', component:HomeComponent},
+  {path : 'heroes', component:HeroesComponent},
+  {path : 'heroe/:id', component:HeroeComponent},
+  {path : 'about', component:AboutComponent},
+  {path : '**', component:HomeComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
